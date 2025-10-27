9d4ffd20c652e02751c0f90a989fbc6f2c8f28fe
akaljot <akaljot4756.be23@chitkara.edu.in>
2025-10-27 10:14:51 +0530

    Lab Collection View

---
+++ collection/collection/BasicCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BasicCollectionViewCell.swift
+//  collection
+//
+//  Created by Student on 15/10/25.
+//
+
+import UIKit
+
+class BasicCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var collectionLabel: UILabel!
+}
---
+++ sdfge/sdfge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sdfge
+//
+//  Created by Student on 20/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
